f283d5e753e04cca0e785831e145cd741236d5d3
cheong <imcheong001@gmail.com>
2021-08-17 21:41:05 +0900

    vue study

---
+++ client/src/components/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello2">
+    <ol>
+      <li v-for="todo in todos" :key="todo">
+        {{ todo.text }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld2',
+  data () {
+    return {
+      todos: [
+        { text: 'nodejs는' },
+        { text: '어렵지만' },
+        { text: '재미있다' }
+      ]
+    }
+  }
+}
+</script>
