bf123f2af97105acabf6fd438c9d515e35ac20e2
cheong <imcheong001@gmail.com>
2021-08-29 00:38:09 +0900

    add side bar ui

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,52 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <div class="sidebar">
+      <a v-for="key in menu" :key="key" class="button black">
+        {{key}} 
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return {
+      menu : ['Home', 'Security techniques', 'Attack technique', 'Use']
+    }    
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+      font-family: "Montserrat", sans-serif;
+      }
+
+    .sidebar {
+      height:100%; 
+      width:25%; 
+      background-color:#fff; 
+      position:fixed!important; 
+      z-index:1;
+    }
+
+    .button {
+      border:none; 
+      display:inline-block; 
+      vertical-align:middle; 
+      cursor:pointer; 
+      width:100%; 
+      text-align:left; 
+      padding:12px 24px!important; 
+      color:#fff!important; 
+      background-color:#000!important;
+    }
+
+    .button:hover {
+      color:#000!important; 
+      background-color:#ccc!important;
+    }
+    
 </style>
